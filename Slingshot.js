7f3f263d5a564ef8431d02b3357efa49e3d82b12
samhitabisht <68056370+samhitabisht@users.noreply.github.com>
2020-08-30 17:56:14 +0530

    Add files via upload

--- Slingshot.js
+++ Slingshot.js
@@ -3,7 +3,7 @@ class SlingShot{
         var options = {
             bodyA: bodyA,
             pointB: pointB,
-            stiffness: 0.04,
+            stiffness: 0.01,
             length: 10
         }
         this.pointB = pointB
@@ -16,6 +16,10 @@ class SlingShot{
 
     fly(){
         this.sling.bodyA = null;
+    }
+
+    attach(body){
+        this.sling.bodyA= body;
     }
 
     display(){
